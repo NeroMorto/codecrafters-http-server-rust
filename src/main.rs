824a2ef972bc456792b69f4f4ce4e3fd175ab6e3
NeroMorto <ragemorto@gmail.com>
2024-03-18 10:05:20 +0300

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,45 @@
-use std::io::Write;
-// Uncomment this block to pass the first stage
+use std::io::{BufRead, Read, Write};
 use std::net::TcpListener;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+
+    let ok_200: String = "HTTP/1.1 200 OK\r\n\r\n".to_owned();
+    let not_found_404: String = "HTTP/1.1 404 Not Found\r\n\r\n".to_owned();
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
 
-                let ok_200: String = "HTTP/1.1 200 OK\r\n\r\n".to_owned();
-                let response = stream.write(ok_200.as_bytes());
-                match response {
-                    Ok(_) => { println!("Responded successfully"); }
-                    Err(error) => {println!("Error during response: {:?}", error);}
+                // let reader = BufReader::new(stream);
+
+
+                let mut buffer = [0; 1024];
+                stream.read(&mut buffer).unwrap();
+
+                let request_lines = String::from_utf8_lossy(&mut buffer);
+                for line in request_lines.split("\r\n").into_iter() {
+                    match line {
+                        line => {
+                            if line.starts_with("GET / HTTP") {
+                                stream.write(ok_200.as_bytes()).unwrap();
+                                break;
+                            }
+                            stream.write(not_found_404.as_bytes()).unwrap();
+                        }
+                    }
                 }
 
+
+                // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+                // let ok_200: String = "HTTP/1.1 200 OK\r\n\r\n".to_owned();
+                // stream.write(ok_200.as_bytes()).unwrap();
+                // match response {
+                //     Ok(_) => { println!("Responded successfully"); }
+                //     Err(error) => { println!("Error during response: {:?}", error); }
+                // }
             }
             Err(e) => {
                 println!("error: {}", e);
